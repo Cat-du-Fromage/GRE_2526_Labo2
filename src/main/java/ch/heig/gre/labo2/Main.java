77e2629e52a9607276302a614be29847038e419e
RemyBlr <remy.bleuer@hotmail.com>
2026-04-30 09:40:03 +0200

    First version

---
+++ src/main/java/ch/heig/gre/labo2/Main.java
@@ -0,0 +1,82 @@
+package ch.heig.gre.labo2;
+
+import ch.heig.gre.labo2.graph.SSSPResult;
+import ch.heig.gre.labo2.graph.SSSPResult.NegativeCycle;
+import ch.heig.gre.labo2.graph.SSSPResult.ShortestPathTree;
+import ch.heig.gre.labo2.graph.WeightedDigraph;
+import ch.heig.gre.labo2.graph.WeightedDigraphReader;
+import ch.heig.gre.labo2.groupQ.SPFA;
+//import ch.heig.gre.labo2.groupQ.SPFASLF;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String dataDir = "data";
+        File dir = new File(dataDir);
+        File[] files = dir.listFiles((d, name) -> name.endsWith(".txt"));
+
+        for (File file : files) {
+            System.out.println("\nRéseau : " + file.getName());
+            System.out.println("-----------------------------");
+
+            WeightedDigraph graph;
+            try {
+                graph = WeightedDigraphReader.fromFile(file.getPath());
+            } catch (IOException e) {
+                System.err.println("Erreur de lecture de " + file.getName() + ": " + e.getMessage());
+                continue;
+            }
+
+            int source = 0;
+
+            System.out.println("SPFA FIFO");
+            runAlgo(graph, source, new SPFA());
+
+            /*System.out.println("\nSPFA SLF");
+            runAlgorithm(graph, source, new SPFASLF());*/
+        }
+    }
+
+    private static void runAlgo(WeightedDigraph graph, int source, SPFA algo) {
+        SSSPResult result = algo.compute(graph, source);
+        SPFA.Stats stats = algo.getStats();
+
+        System.out.println("Stats :");
+        System.out.println("- Sommets retirés de la file : " + stats.nbrRemovedFromQueue());
+        System.out.println("- Arcs examinés              : " + stats.nbrExaminedArcs());
+        System.out.println("- Relaxations réussies       : " + stats.nbrRelaxations());
+        System.out.println("- Mise en file               : " + stats.nbrEnqueues());
+        System.out.printf("Temps d'exéc : %.2f ms%n", stats.execTimeInMs() / 1_000_000.0);
+
+        int n = graph.getNVertices();
+        if (result.isNegativeCycle()) {
+            NegativeCycle negativeCycle = result.getNegativeCycle();
+            System.out.println("Cycle de poids négatif détecté : ");
+            System.out.println("- Poids total       : " + negativeCycle.length());
+            System.out.println("- Cycle des sommets : " + negativeCycle.values());
+        } else {
+            ShortestPathTree shortestPathTree = result.getShortestPathTree();
+            if (n < 25) {
+                System.out.println("Arbre des plus courts chemins de source -> " + shortestPathTree.source() + " : ");
+                System.out.print("- Distances:      [");
+                for (int i = 0; i < shortestPathTree.distances().length; i++) {
+                    if (i > 0) System.out.print(", ");
+                    int d = shortestPathTree.distances()[i];
+                    System.out.print(d == Integer.MAX_VALUE ? "infini" : d);
+                }
+                System.out.println("]");
+                System.out.print("- Predecesseurs:  [");
+                for (int i = 0; i < shortestPathTree.predecessors().length; i++) {
+                    if (i > 0) System.out.print(", ");
+                    int p = shortestPathTree.predecessors()[i];
+                    System.out.print(p == -1 ? "null" : p);
+                }
+                System.out.println("]");
+            } else {
+                System.out.println("Arbre de plus court chemin trouvé (graphe >= 25 sommets)");
+            }
+        }
+    }
+}
--- src/main/java/ch/heig/gre/labo2/groupQ/SPFA.java
+++ src/main/java/ch/heig/gre/labo2/groupQ/SPFA.java
@@ -4,14 +4,159 @@ import ch.heig.gre.labo2.graph.SSSPAlgorithm;
 import ch.heig.gre.labo2.graph.SSSPResult;
 import ch.heig.gre.labo2.graph.WeightedDigraph;
 
-/**
- * Shortest Path Faster Algorithm (SPFA).
- */
+import java.util.*;
+
 public class SPFA implements SSSPAlgorithm {
 
+  private Stats stats;
+
+  /**
+   * Stats of the SPFA algorithm execution.
+   * @param nbrRemovedFromQueue Le nombre total de sommets retirés de la file
+   * @param nbrExaminedArcs Le nombre d’arcs examinés
+   * @param nbrRelaxations Le nombre de relaxations réussies
+   * @param nbrEnqueues  Le nombre de mises en file
+   * @param execTimeInMs Temps d’exécution total
+   */
+  public record Stats(int nbrRemovedFromQueue,
+                      int nbrExaminedArcs,
+                      int nbrRelaxations,
+                      int nbrEnqueues,
+                      long execTimeInMs) {}
+
+  public Stats getStats() {return stats;}
 
   @Override
   public SSSPResult compute(WeightedDigraph graph, int from) {
-    return null;
+
+    final int n = graph.getNVertices();
+    // On divise par 2 pour éviter un potentiel overflow
+    final int INFINITY = Integer.MAX_VALUE / 2;
+    int[] distance = new int[n];
+    int[] predecessor = new int[n];
+    int[] count = new int[n];
+    boolean[] isInQueue = new boolean[n];
+    // Poids de l'arc qui a permis d'atteindre le sommet
+    int[] arcWeight = new int[n];
+
+    // Toutes les distances à l'infini
+    Arrays.fill(distance, INFINITY);
+    // Tous les prédécesseurs à -1
+    Arrays.fill(predecessor, SSSPResult.UNREACHABLE);
+    // Sommet de départ à 0
+    distance[from] = 0;
+    // File FIFO
+    Deque<Integer> queue = new ArrayDeque<>();
+    queue.add(from);
+    isInQueue[from] = true;
+    count[from]++;
+
+    int removed = 0;
+    int exminedArcs = 0;
+    int relaxations = 0;
+    int enqueues = 1;
+
+    long start = System.nanoTime();
+
+    while (!queue.isEmpty()) {
+      // Rtirer le premier élément de la queue (FIFO)
+      int u = queue.poll();
+      isInQueue[u] = false;
+      removed++;
+
+      // Pour chaque arc (u, v) avec poids c(u, v)
+      for (WeightedDigraph.Edge edge : graph.getOutgoingEdges(u)) {
+        exminedArcs++;
+        int v = edge.to();
+        int weight = edge.weight();
+
+        if (distance[v] > distance[u] + weight /*&& distance[u] != INFINITY*/) {
+          distance[v] = distance[u] + weight;
+          predecessor[v] = u;
+          arcWeight[v] = weight;
+          relaxations++;
+
+          if (!isInQueue[v]) {
+            // FIFO donc on ajoute à la fin de la queue
+            queue.addLast(v);
+            isInQueue[v] = true;
+            enqueues++;
+            count[v]++;
+
+            if (count[v] >= n) {
+              long elapsedTime = System.nanoTime() - start;
+              stats = new Stats(removed, exminedArcs, relaxations, enqueues, elapsedTime);
+              List<Integer> cycle = buildNegativeCycle(predecessor, v, n);
+              int totalWeight = getCycleWeigt(cycle, arcWeight);
+
+              // Retourner un circuit absorbant accessible depuis s détecté
+              return new SSSPResult.NegativeCycle(cycle, totalWeight);
+            }
+          }
+        }
+      }
+    }
+
+    long elapsedTime = System.nanoTime() - start;
+    stats = new Stats(removed, exminedArcs, relaxations, enqueues, elapsedTime);
+
+    // Retourner l'arbre des plus courts chemins de source -> from
+    return new SSSPResult.ShortestPathTree(from, distance, predecessor);
+  }
+
+  /**
+   * Calcule le poids total d'un cycle à partir de la liste de ses sommets et du tableau des poids des arcs.
+   * @param cycle Liste des sommets formant le cycle, dans l'ordre du cycle
+   * @param arcWeight Tableau des poids des arcs qui ont permis d'atteindre chaque sommet du cycle
+   * @return Le poids total du cycle
+   */
+  private int getCycleWeigt(List<Integer> cycle, int[] arcWeight) {
+    int total = 0;
+
+    // On commence à 1 pour ne pas compter le poids de l'arc qui arrive au premier sommet du cycle
+    for (int i = 1; i < cycle.size(); i++)
+      // On ajoute le poids de l'arc qui a permis d'atteindre le sommet du cycle
+      total += arcWeight[cycle.get(i)];
+
+    return total;
+  }
+
+  /**
+   * Construit un cycle de poids négatif à partir du tableau des prédécesseurs.
+   * @param pred Tableau des prédécesseurs de chaque sommet dans l'arbre des plus courts chemins
+   * @param start Sommet à partir duquel on a détecté le cycle de poids négatif
+   *              (le sommet qui a été inséré dans la queue n fois)
+   * @param n Nombre de sommets dans le graphe
+   * @return Une liste de sommets formant un cycle de poids négatif accessible depuis le sommet
+   * de départ, dans l'ordre du cycle (le premier sommet est répété à la fin pour fermer le cycle)
+   */
+  private List<Integer> buildNegativeCycle(int[] pred, int start, int n) {
+    // Sommet qui appartient à un cycle en remontant les predécesseurs à partir de start
+    // Comme on a inséré un sommet n fois, il y a aun moins un cycle parmis les predécesseurs
+    boolean[] visited = new boolean[n];
+    int current = start;
+
+    // On remonte les précdecesseurs jusqu'à trouver un sommet déjà visité
+    // Signifie qu'on a trouvé un cycle
+    while (!visited[current]) {
+      visited[current] = true;
+      current = pred[current];
+    }
+
+    // Premier sommet visité deux fois
+    int firstVertice = current;
+    List<Integer> cycle = new ArrayList<>();
+    cycle.add(firstVertice);
+    // On parcourt le circuit une première fois pour collecter ses sommet
+    // On acance sur les predécesseurs jusqu'à retrouver le premier sommet du cycle
+    for (int v = pred[firstVertice]; v != firstVertice; v = pred[v])
+      cycle.add(v);
+
+    // On ajoute le premier cycle à la fin pour terminer le cycle
+    cycle.add(firstVertice);
+
+    // On remonte les predécesseurs donc on a la séquence dans l'ordre inverse
+    Collections.reverse(cycle);
+    return cycle;
   }
 }
